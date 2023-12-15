0b385ebeff056482b0543f3879152eaf163c45b7
Misha <mkrusharska@gmail.com>
2023-12-15 17:29:59 +0200

    Port from env file

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const jsonServer = require('json-server');
 const server = jsonServer.create();
 const router = jsonServer.router('db');
 const middlewares = jsonServer.defaults();
-const PORT = 8000;
+const PORT = process.env.PORT || 8000;
 
 server.use(middlewares);
 server.use(router);
