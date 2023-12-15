7aaf621b3f00165d9080263b4c65e7e4cb7e5dc9
Misha <mkrusharska@gmail.com>
2023-12-15 15:50:07 +0200

    json

---
+++ server.js
@@ -0,0 +1,12 @@
+const jsonServer = require('json-server');
+const server = jsonServer.create();
+const router = jsonServer.router('db');
+const middlewares = jsonServer.defaults();
+const port = process.env.PORT || 8000;
+
+server.use(middlewares);
+server.use(router);
+
+server.listen(PORT, () => {
+  console.log(`JSON Server is running on http://localhost:${PORT}`);
+});
